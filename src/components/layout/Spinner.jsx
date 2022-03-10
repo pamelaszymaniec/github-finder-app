bfea982a38329e00e8fb511d6bb146e6826366d4
Pamela <pamela.szymaniec@gmail.com>
2022-03-10 16:05:54 +0100

    Setting up Users component

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,24 @@
+import "./spinner.css";
+
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <div className="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
