3d7707680bcc2322618df2d95e0e77190a1e94ad
Pamela <pamela.szymaniec@gmail.com>
2022-03-10 19:03:00 +0100

    Alerts and getting single user

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,16 +1,20 @@
 import { useState, useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContex";
 
 function UserSearch() {
   const [text, setText] = useState("");
+
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
+
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (text === "") {
-      alert("Please enter sth...");
+      setAlert("Please enter something", "error");
     } else {
-      //@todo seatch users
       searchUsers(text);
       setText("");
     }
